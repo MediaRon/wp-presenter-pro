1ae6d2e8a6ae9982b4415074ee36ee734f25cafc
Ronald Huereca <rhuereca@updraftplus.com>
2020-01-02 07:08:02 -0600

    Abstracting out fonts so others can add to them. Adding Typekit support.

--- src/block/blocks/fonts/fonts.js
+++ src/block/blocks/fonts/fonts.js
@@ -1,18 +1,10 @@
 const { __, _x } = wp.i18n;
-const wpPresentationFonts = [
-	{ value: 'bubblebum', label: __('Bubblegum', 'wp-presenter-pro') },
-	{ value: 'dosis', label: __('Dosis', 'wp-presenter-pro') },
-	{ value: 'encode', label: __('Encode', 'wp-presenter-pro') },
-	{ value: 'lato', label: __('Lato', 'wp-presenter-pro') },
-	{ value: 'league', label: __('League', 'wp-presenter-pro') },
-	{ value: 'libre-baskerville', label: __('Libre Baskerville', 'wp-presenter-pro') },
-	{ value: 'merriweather', label: __('Merriweather', 'wp-presenter-pro') },
-	{ value: 'montserrat', label: __('Montserrat', 'wp-presenter-pro') },
-	{ value: 'news-cycle', label: __('News Cycle', 'wp-presenter-pro') },
-	{ value: 'open-sans', label: __('Open Sans', 'wp-presenter-pro') },
-	{ value: 'quicksand', label: __('Quicksand', 'wp-presenter-pro') },
-	{ value: 'sinkins-sans', label: __('Sinkins Sans', 'wp-presenter-pro') },
-	{ value: 'Source Sans Pro', label: __('Source Sans Pro', 'wp-presenter-pro') },
-	{ value: 'ubuntu', label: __('Ubuntu', 'wp-presenter-pro') },
-]
-export default wpPresentationFonts;
+let wpPresenterProFonts = [];
+const presenterFonts = Object.entries(wp_presenter_pro.fonts);
+for (const key of presenterFonts) {
+	wpPresenterProFonts.push( {
+		value: key[0],
+		label: key[1],
+	} );
+}
+export default wpPresenterProFonts;
