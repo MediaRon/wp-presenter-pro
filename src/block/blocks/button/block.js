455efdf7f9121e3a2c6ed81cd6fe79f740e9bbfe
Ronald Huereca <rhuereca@updraftplus.com>
2020-01-02 22:27:53 -0600

    Beginning of button component.

---
+++ src/block/blocks/button/block.js
@@ -0,0 +1,28 @@
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { Component, Fragment } = wp.element;
+import edit from './edit';
+
+/**
+ * Register Basic Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made available as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'wppp/button', {
+	title: __( 'Content', 'wp-presenter-pro' ), // Block title.
+	icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 5H5c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 12H5V7h14v10z"/></svg>,
+	category: 'wp-presenter-pro',
+	keywords: [
+		__( 'slide', 'wp-presenter-pro' ),
+		__( 'button', 'wp-presenter-pro' ),
+	],
+	edit: edit,
+	save() {return null }
+} );
