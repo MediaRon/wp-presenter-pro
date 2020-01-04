5967cfa434c5ef932c5094f0e43c358cd7a898cc
Ronald Huereca <rhuereca@updraftplus.com>
2020-01-04 14:10:24 -0600

    Allowing all blocks to be disabled/enabled.

--- src/block/blocks/allowed-blocks/allowed-blocks.js
+++ src/block/blocks/allowed-blocks/allowed-blocks.js
@@ -1,3 +1,6 @@
 const { __, _x } = wp.i18n;
-const ALLOWED_BLOCKS = wp_presenter_pro.allowed_blocks;
+let ALLOWED_BLOCKS = wp_presenter_pro.allowed_blocks;
+if ( 'all' === wp_presenter_pro.block_options ) {
+	ALLOWED_BLOCKS = true;
+}
 export default ALLOWED_BLOCKS;
