0717a76a56f3db7f9077c372d4728c83c281b097
Ronald Huereca <rhuereca@updraftplus.com>
2020-02-01 14:05:26 -0600

    Updating version.

--- wp-presenter-pro.php
+++ wp-presenter-pro.php
@@ -9,7 +9,7 @@
  * Plugin Name: WP Presenter Pro
  * Plugin URI: https://mediaron.com/wp-presenter-pro-documentation/
  * Description: A plugin for allowing you to do presentations using WordPress.
- * Version: 3.1.1
+ * Version: 3.1.2
  * Author: MediaRon LLC
  * Author URI: https://mediaron.com
  * License: GPL2
@@ -22,7 +22,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-define( 'WP_PRESENTER_PRO_VERSION', '3.1.1' );
+define( 'WP_PRESENTER_PRO_VERSION', '3.1.2' );
 define( 'WP_PRESENTER_PRO_PLUGIN_NAME', 'WP Presenter Pro' );
 define( 'WP_PRESENTER_PRO_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WP_PRESENTER_PRO_URL', plugins_url( '/', __FILE__ ) );
